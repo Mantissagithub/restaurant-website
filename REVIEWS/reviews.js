3a8432064974d38051c9d5d86bb0fd7d851b9117
Pradheep P <vidhyaprakash2510@gmail.com>
2023-11-19 00:31:24 +0530

    file structuring

---
+++ REVIEWS/reviews.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//     if ($('.reviews-slider').length > 0) {
+//         $('.reviews-slider').slick({
+//             dots: true,
+//             infinite: true,
+//             slidesToShow: 1,
+//             slidesToScroll: 1,
+//             autoplay: true,
+//             autoplaySpeed: 2000 
+//         });
+//     } else {
+//         console.error("There's no element with the class 'reviews-slider'");
+//     }
+// });
+
+// // Additional fix to prevent unwanted horizontal scrolling due to slider
+// $('.reviews-slider').on('init', function(event, slick){
+//     $('body').css('overflow-x', 'hidden');
+// });
